726c054c11797a2010aee77c8a67107829fed45d
jnabnet <jnabnet@gmail.com>
2016-08-16 06:19:14 -0700

    Basic work arounds

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+    'author': "Jnabnet",
+    'website': "https://www.jnab.net",
+    'name': 'Boot Lumenet',
+    'summary': 'Boot Lumenet',
+    'description': Boot Lumenet',
+    'category': 'Theme',
+    'version': '0.4',
+    'images': ['images/lumen-thumbnail.png', 'themelumen.png'],
+    'depends': ['website'],
+    'data': ['views/bootswatch-lumen.xml'],
+    'application': False,
+}
